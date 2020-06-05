32ff172a058b36e3610545303faf97efe29424e2
syafzid <18052270@myrp.edu.sg>
2020-06-05 11:03:43 +0800

    Section A and B of Worksheet are completed

---
+++ app/src/main/java/com/example/demofragment/FirstFragment.java
@@ -0,0 +1,37 @@
+package com.example.demofragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FirstFragment extends Fragment {
+
+    Button btnAddText;
+    TextView tvFrag1;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        tvFrag1 = view.findViewById(R.id.tvFrag1);
+        btnAddText = view.findViewById(R.id.btnAddFrag1);
+
+        btnAddText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = tvFrag1.getText().toString() + "\n" + "New Data";
+                tvFrag1.setText(data);
+            }
+        });
+
+        return view;
+    }
+}
